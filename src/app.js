7f20f1c8f07de406d12d645c923f23805a0affa6
drkchiloll <samwomack_@me.com>
2015-10-02 18:11:32 -0500

    List of Pages added to the view..until Data is received from the server (Firebase) Loading.. displays on the screen..

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Router from 'react-router';
 import App from './components/App';
 
+
 const {Route} = Router;
 
 var routes = (
   <Route handler={App}>
+    <Route name='page' path='/page/:id' handler={App}/>
   </Route>
 );
 
